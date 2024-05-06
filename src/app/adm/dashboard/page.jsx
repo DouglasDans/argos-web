e4f4f431fa24a0a25820bd4d8984ee5ce8db33f6
douglasdans <douglas.dans@gmail.com>
2024-05-05 23:54:39 -0300

    Adm Dashboard | Adiciona links dos botões de gerenciamento do sistema

--- src/app/adm/dashboard/page.jsx
+++ src/app/adm/dashboard/page.jsx
@@ -2,6 +2,8 @@ import {Typography} from "@mui/joy";
 import React, {Fragment} from "react";
 
 import styles from './page.module.css';
+import ContainerLevel1 from "@/ui/containers/ContainerLevel1";
+import LinkButton from "@/ui/LinkButton";
 
 export default function Dashboard(){
    return (
@@ -13,11 +15,52 @@ export default function Dashboard(){
 
          <div className='flex flex-col gap-2'>
             <div className={styles.dashContainer}>
-               <div className={styles.buttonsContainer}>
-                  aa
+               <div className={styles.buttonsContainer + ' flex flex-col gap-2'}>
+                  <LinkButton
+                     href="chamados"
+                     ariaLabel="Botão para gerenciar chamados pendentes"
+                     variant="solid"
+                  >
+                     Chamados Pendentes
+                  </LinkButton>
+
+                  <LinkButton
+                     href="responsaveis"
+                     ariaLabel="Botão para gerenciar Gerenciar Responsáveis"
+                     variant="solid"
+                  >
+                     Gerenciar Responsáveis
+                  </LinkButton>
+
+                  <LinkButton
+                     href="dependentes"
+                     ariaLabel="Botão para gerenciar dependentes"
+                     variant="solid"
+                  >
+                     Gerenciar Dependentes
+                  </LinkButton>
+
+                  <LinkButton
+                     href="trancas"
+                     ariaLabel="Botão para gerenciar trancas"
+                     variant="solid"
+                  >
+                     Gerenciar Trancas
+                  </LinkButton>
+
+                  <LinkButton
+                     href="historico"
+                     ariaLabel="Botão para acessar histórico"
+                     variant="solid"
+                  >
+                     Histórico Geral
+                  </LinkButton>
+
                </div>
                <div className={styles.activityContainer}>
-                  bb
+                  <ContainerLevel1>
+                     aa
+                  </ContainerLevel1>
                </div>
                <div className={styles.chamadosContainer}>
                   cc
