fe010a49cc4ef1ece4f52e0590a168133a497178
douglasdans <douglas.dans@gmail.com>
2024-05-05 23:26:35 -0300

    Adm Dashboard | Adiciona estrutura do grid da página

--- src/app/adm/dashboard/page.jsx
+++ src/app/adm/dashboard/page.jsx
@@ -1,8 +1,34 @@
+import {Typography} from "@mui/joy";
+import React, {Fragment} from "react";
+
+import styles from './page.module.css';
 
 export default function Dashboard(){
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+   return (
+      <Fragment>
+         <div className='title'>
+            <Typography level={'h2'}>Dashboard</Typography>
+            <Typography level={'body-sm'}></Typography>
+         </div>
+
+         <div className='flex flex-col gap-2'>
+            <div className={styles.dashContainer}>
+               <div className={styles.buttonsContainer}>
+                  aa
+               </div>
+               <div className={styles.activityContainer}>
+                  bb
+               </div>
+               <div className={styles.chamadosContainer}>
+                  cc
+               </div>
+            </div>
+
+            <div>
+               dd
+            </div>
+
+         </div>
+      </Fragment>
+   )
 }
