7ca23cd3d95f76e31030a981c5fde53766ba2abe
douglasdans <douglas.dans@gmail.com>
2024-05-05 23:24:31 -0300

    Chamado | Adiciona botão de retornar página

--- src/app/user/chamado/page.jsx
+++ src/app/user/chamado/page.jsx
@@ -1,13 +1,20 @@
 import React, {Fragment} from "react";
 import {Typography} from "@mui/joy";
 import FormChamado from "@/ui/dashboard/FormChamado/FormChamado";
+import {ArrowBack} from "@mui/icons-material";
+import Link from "next/link";
 
 export default function ChamadoPage() {
    return (
       <Fragment>
-         <div className='title'>
-            <Typography level={'h2'}>Dashboard</Typography>
-            <Typography level={'body-md'}>Dashboard/Solicitar Chamado</Typography>
+         <div className='title flex flex-col'>
+            <div className={'flex items-center gap-3'}>
+               <Link aria-label='Link de retorno para a tela principal da dashboard'  Link href={'/user/dashboard'}>
+                  <ArrowBack/>
+               </Link>
+               <Typography level={'h2'}>Dashboard</Typography>
+            </div>
+            <Typography level={'body-md'} className={'pl-9'} >Dashboard/Solicitar Chamado</Typography>
          </div>
 
          <div className={'flex justify-center items-center'}>
