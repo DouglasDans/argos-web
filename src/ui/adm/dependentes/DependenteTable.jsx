ac69649c6f27e6b378be78643f7cf61ed20976f0
douglasdans <douglas.dans@gmail.com>
2024-05-10 15:04:40 -0300

    Adm Dashboard | Adiciona form e table dos dependentes

---
+++ src/ui/adm/dependentes/DependenteTable.jsx
@@ -0,0 +1,46 @@
+import ContainerLevel1 from "@/ui/containers/ContainerLevel1";
+import LinkButton from "@/ui/LinkButton";
+import {Button, Table} from "@mui/joy";
+import {Delete, Edit} from "@mui/icons-material";
+import React from "react";
+
+export default function DependenteTable(){
+   return (
+      <ContainerLevel1 className="p-4 flex flex-col gap-4">
+         <div className={'w-5'}>
+            <LinkButton href={'/adm/dependentes/adicionar'}>Adicionar Dependentes</LinkButton>
+         </div>
+
+         <Table>
+            <thead>
+               <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>RG</th>
+                  <th>Apto</th>
+                  <th>Editar</th>
+                  <th>Excluir</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>2132</td>
+                  <td>Fulano</td>
+                  <td>243.234.23</td>
+                  <td>1-A</td>
+                  <td>
+                     <Button aria-label='Botão para acessar histórico da TAG' color='neutral' variant='soft'>
+                        <Edit/>
+                     </Button>
+                  </td>
+                  <td>
+                     <Button aria-label='Botão para acessar histórico da TAG' color='neutral' variant='soft'>
+                        <Delete/>
+                     </Button>
+                  </td>
+               </tr>
+            </tbody>
+         </Table>
+      </ContainerLevel1>
+   )
+}
