ef868a87040b7db7e96426429ebe7f1f92ba301a
douglasdans <douglas.dans@gmail.com>
2024-05-10 15:04:29 -0300

    Adm Dashboard | Adiciona form e table dos dependentes

---
+++ src/app/adm/dependentes/adicionar/page.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import {ArrowBack} from "@mui/icons-material";
+import {Typography} from "@mui/joy";
+import FormAddResponsavel from "@/ui/adm/responsaveis/FormAddResponsavel";
+import {Fragment} from "react";
+import FormAddDependente from "@/ui/adm/dependentes/FormAddDependente";
+
+export default function AdicionarDependente() {
+   return (
+      <Fragment>
+         <div className='title flex flex-col'>
+            <div className={'flex items-center gap-3'}>
+               <Link aria-label='Link de retorno para a tela principal da dashboard'  Link href={'/adm/responsaveis'}>
+                  <ArrowBack/>
+               </Link>
+               <Typography level={'h2'}>Dashboard</Typography>
+            </div>
+            <Typography level={'body-md'} className={'pl-9'}>Dashboard / Gerenciar Dependentes/ Adicionar Dependente</Typography>
+         </div>
+         <FormAddDependente/>
+      </Fragment>
+   )
+}
