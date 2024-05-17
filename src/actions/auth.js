6db5b4aade386d42afb63133692cfa8f9da23a5c
douglasdans <douglas.dans@gmail.com>
2024-05-16 23:28:21 -0300

    Adiciona autenticação com dados puxados do backend

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,14 +3,30 @@ import {redirect} from "next/navigation";
 export default async function authAction(formData){
    'use server'
 
-   const responseFormData = {
+   const requestFormData = {
       idAcesso: formData.get('txtIdAcesso'),
       senha: formData.get('txtSenha')
    }
 
-   if (responseFormData.idAcesso === 'admin' && responseFormData.senha === 'admin') {
-      redirect('/adm/dashboard')
+   const response = await fetch('http://localhost:8080/api/v1/auth', {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         'acessoId': requestFormData.idAcesso,
+         'senha': requestFormData.senha
+      }
+   })
+
+   const responseAuth = await response.json()
+
+   if (responseAuth.auth) {
+      if (responseAuth.type === 'administrador') {
+         redirect('/adm/dashboard')
+      }
+      if (responseAuth.type === 'responsavel') {
+         redirect('/user/dashboard')
+      }
    } else {
-      redirect('user/dashboard')
+      redirect('/')
    }
 }
