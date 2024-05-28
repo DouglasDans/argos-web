62f75e58872cc452e7f65440510fb1a57e85e9b2
douglasdans <douglas.dans@gmail.com>
2024-05-28 17:02:33 -0300

    Adiciona url global a todas as instâncias do Axios

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,69 +1,7 @@
-const hostPath = "http://localhost:8080/"
-const apiPath = "api/v1/"
+const { default: axios } = require("axios");
 
-const BASE_URL = hostPath + apiPath
+const apiRequest = axios.create({
+   baseURL: 'http://localhost:8080/api/v1/'
+ });
 
-export async function get(endpoint) {
-   try {
-      const resposta = await fetch(BASE_URL + endpoint);
-      const data = await resposta.json();
-
-      return {
-         data: data
-      }
-      
-   } catch (error) {
-      return{
-         error: error.message
-      }
-   }
-}
-
-export async function post(endpoint, body) {
-
-   try {
-      let inserir = await fetch(BASE_URL + endpoint, {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(body)
-      })
-
-      const dados = await inserir.json();
-
-      return  {
-         dados: dados
-      }
-   } catch (error) {
-      console.log();
-      return {
-         error: error.message
-      }
-   }
-}
-
-export async function patch(endpoint, body) {
-
-   try {
-      let inserir = await fetch(BASE_URL + endpoint, {
-         method: "PATCH",
-         headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(body)
-      })
-
-      const dados = await inserir.json();
-
-      return  {
-         dados: dados
-      }
-   } catch (error) {
-      console.log();
-      return {
-         error: error.message
-      }
-   }
-}
-
+export default apiRequest
