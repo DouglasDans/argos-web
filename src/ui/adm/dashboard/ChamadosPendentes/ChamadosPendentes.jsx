bab1525f5acbdc652b1df860127835bc225276d6
douglasdans <douglas.dans@gmail.com>
2024-06-03 18:49:57 -0300

    Corrige a exibição do histórico da tag

--- src/ui/adm/dashboard/ChamadosPendentes/ChamadosPendentes.jsx
+++ src/ui/adm/dashboard/ChamadosPendentes/ChamadosPendentes.jsx
@@ -4,40 +4,43 @@ import {Button, Typography} from "@mui/joy";
 import { ContainerLevel3 } from "@/ui/containers/ContainerLevel3";
 
 export default function ChamadosPendentes() {
-   return (
-      <ContainerLevel1 className={'h-full w-full p-4 flex flex-col gap-3'}>
-         <div className="flex justify-between items-center">
-            <Typography level={'title-lg'}>Chamados Pendentes</Typography>
-            <Button color="neutral">Ver Mais</Button>
-         </div>
 
-         <div className={'flex felx-col'}>
-            <ContainerLevel3 className="flex justify-between w-full">
-               <div className="flex flex-col">
-                  <Typography level="title-md">Cadastro de Tag</Typography>
-                  <Typography level="body-sm">Tipo: Permanente</Typography>
-                  <Typography level="body-sm">Responsável: Fulado</Typography>
-               </div>
+   return("")
 
-               <div className="flex flex-col justify-center">
-                  <Button color="neutral">Mais Info.</Button>
-               </div>
-            </ContainerLevel3>
-         </div>
-         <div className={'flex felx-col'}>
-            <ContainerLevel3 className="flex justify-between w-full">
-               <div className="flex flex-col">
-                  <Typography level="title-md">Cadastro de Tag</Typography>
-                  <Typography level="body-sm">Tipo: Permanente</Typography>
-                  <Typography level="body-sm">Responsável: Fulado</Typography>
-               </div>
+   // return (
+   //    <ContainerLevel1 className={'h-full w-full p-4 flex flex-col gap-3'}>
+   //       <div className="flex justify-between items-center">
+   //          <Typography level={'title-lg'}>Chamados Pendentes</Typography>
+   //          <Button color="neutral">Ver Mais</Button>
+   //       </div>
 
-               <div className="flex flex-col justify-center">
-                  <Button color="neutral">Mais Info.</Button>
-               </div>
-            </ContainerLevel3>
-         </div>
+   //       <div className={'flex felx-col'}>
+   //          <ContainerLevel3 className="flex justify-between w-full">
+   //             <div className="flex flex-col">
+   //                <Typography level="title-md">Cadastro de Tag</Typography>
+   //                <Typography level="body-sm">Tipo: Permanente</Typography>
+   //                <Typography level="body-sm">Responsável: Fulado</Typography>
+   //             </div>
 
-      </ContainerLevel1>
-   )
+   //             <div className="flex flex-col justify-center">
+   //                <Button color="neutral">Mais Info.</Button>
+   //             </div>
+   //          </ContainerLevel3>
+   //       </div>
+   //       <div className={'flex felx-col'}>
+   //          <ContainerLevel3 className="flex justify-between w-full">
+   //             <div className="flex flex-col">
+   //                <Typography level="title-md">Cadastro de Tag</Typography>
+   //                <Typography level="body-sm">Tipo: Permanente</Typography>
+   //                <Typography level="body-sm">Responsável: Fulado</Typography>
+   //             </div>
+
+   //             <div className="flex flex-col justify-center">
+   //                <Button color="neutral">Mais Info.</Button>
+   //             </div>
+   //          </ContainerLevel3>
+   //       </div>
+
+   //    </ContainerLevel1>
+   // )
 }
